e287ed4d611de322d5102599784cceca30e61d94
Veronica Martinez <a01275996@itesm.mx>
2021-03-04 20:12:42 -0600

    EXAMEN

---
+++ src/Page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Welcome from './images/welcome.jpeg';
+
+function Page(){
+    return (
+        <section className="Page">
+            <img> </img>
+            <h1>Ayra Stark</h1>
+            <p>PLAYED BY MAISIE WILLIAMS</p>
+            <p>The younger of Ned and Cat's two daughters. A tomboy, she would rather fence than dance. Her direwolf is named Nymeria, after a warrior queen.</p>
+        </section>
+
+    )
+}
+
+export default Page;
